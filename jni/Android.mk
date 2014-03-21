2f5c2b5524efc69cd912afbfd1e1bed70b3d9b9f
Poly Takahiro Horikawa <horikawa.takahiro@gmail.com>
2014-03-20 23:31:43 -0700

    - Add compile option - Compile with NEON - Update OpenCV version - Extract environment specific path

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,11 +6,13 @@ OPENCV_CAMERA_MODULES:=on
 OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
 
-include /Users/poly/workspace/OpenCV-2.4.6-android-sdk/sdk/native/jni/OpenCV.mk
+include $(OPENCV_ANDROID_SDK_HOME)/OpenCV.mk
 
 LOCAL_SRC_FILES  := Scouter.cpp jni_scouter.cpp preprocessFace.cpp detectObject.cpp
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_CFLAGS    := -Werror -O3 -ffast-math
 LOCAL_LDLIBS     += -llog -ldl
+LOCAL_ARM_NEON := true
 LOCAL_MODULE     := scouter
 LOCAL_SHARED_LIBRARIES +=
 
